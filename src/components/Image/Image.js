f066f00be516e2ee7d6a82a73305093991f77741
dimitrisz123 <dimitris-007@hotmail.com>
2018-07-08 18:42:30 +0300

    new image

--- src/components/Image/Image.js
+++ src/components/Image/Image.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './Image.css';
+import sample from './sample.jpg';
 
 
 const Image = () => {
 	return (
 	<div>
 		<div className = 'center'>
-			<img alt = 'sample' src = {'https://cdn.wallpapersafari.com/51/84/CJoRwO.jpg'} width = 'auto' height = '300px'/>
+			<img alt = 'sample' src = {sample} width = 'auto' height = '300px'/>
 		</div>
 	</div>
 		) 
