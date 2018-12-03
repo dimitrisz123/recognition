5810f9a9a2c001c7f7475f27dd64b20f315fc259
GH_NAME <GH_EMAIL>
2018-12-03 21:37:09 +0000

    Updates

---
+++ precache-manifest.3620957ee2a4082574afad5309161211.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "35b19bd5a4fded9ad227",
+    "url": "/foodrecognition/static/js/runtime~main.35b19bd5.js"
+  },
+  {
+    "revision": "b9bd3662f829d7872216",
+    "url": "/foodrecognition/static/js/main.b9bd3662.chunk.js"
+  },
+  {
+    "revision": "088dbf780aee39ea7e04",
+    "url": "/foodrecognition/static/js/1.088dbf78.chunk.js"
+  },
+  {
+    "revision": "b9bd3662f829d7872216",
+    "url": "/foodrecognition/static/css/main.69442126.chunk.css"
+  },
+  {
+    "revision": "bd33fe4930b616121902df61b2da5f3a",
+    "url": "/foodrecognition/index.html"
+  }
+];
